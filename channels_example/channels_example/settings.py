cd0c09c8187eb571694f6c81d0d1bcaaefb67169
yangjin <13246856469@163.com>
2018-01-23 20:20:51 +0800

    add

--- channels_example/channels_example/settings.py
+++ channels_example/channels_example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=4cq8(hi7=uwez84915ckx^$39c+4k*@&2*mj8!%05%jrukxcl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['47.94.158.171']
 
 
 # Application definition
