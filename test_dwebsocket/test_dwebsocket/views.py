cfcec1ac828f94351397ca7febc474aece19ad2c
yangjin <13246856469@163.com>
2018-01-24 17:46:36 +0800

    modify

--- test_dwebsocket/test_dwebsocket/views.py
+++ test_dwebsocket/test_dwebsocket/views.py
@@ -4,9 +4,19 @@ __author__ = 'liyangjin'
 __time__ = '2018/1/24 16:43'
 
 		
-from dwebsocket import require_websocket
+from django.shortcuts import render
+from dwebsocket.decorators import accept_websocket,require_websocket
+from django.http import HttpResponse
 
-@require_websocket
-def channels_example(request):
-    message = request.websocket.wait()
-    request.websocket.send(message)
+
+@accept_websocket
+def echo(request):
+    if not request.is_websocket():#判断是不是websocket连接
+        try:#如果是普通的http方法
+            message = request.GET['message']
+            return HttpResponse(message)
+        except:
+            return render("hello")
+    else:
+        for message in request.websocket:
+            request.websocket.send(message)#发送消息到客户端
